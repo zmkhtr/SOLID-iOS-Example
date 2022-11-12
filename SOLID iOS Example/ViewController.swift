de98d7b37afabfecda11668a1b3ef317cef9a74f
zmkhtr <azammukhtar3@gmail.com>
2022-11-12 13:39:05 +0700

    Initial Commit

---
+++ SOLID iOS Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOLID iOS Example
+//
+//  Created by Azam Mukhtar on 12/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
