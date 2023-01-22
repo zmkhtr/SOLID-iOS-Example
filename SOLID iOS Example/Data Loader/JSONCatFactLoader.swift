d26a03473b2df623d3fee70dbe4cf306692f616d
zmkhtr <azammukhtar3@gmail.com>
2023-01-22 22:10:20 +0700

    Refactor JSONCatFactLoader load implementation for making sure all cases catch in completion

--- SOLID iOS Example/Data Loader/JSONCatFactLoader.swift
+++ SOLID iOS Example/Data Loader/JSONCatFactLoader.swift
@@ -9,18 +9,20 @@ import Foundation
 
 class JSONCatFactLoader: CatFactLoader {
     override func load(completion: @escaping (CatFactLoader.Result) -> Void) {
-        if let url = Bundle.main.url(forResource: "CatFacts", withExtension: "json") {
-            do {
-                let data = try Data(contentsOf: url)
-                let decoder = JSONDecoder()
-                let jsonData = try decoder.decode(RemoteCatFactResponse.self, from: data)
-                let catFacts = jsonData.data.map { remoteFact in
-                    CatFactItem(fact: remoteFact.fact)
-                }
-                completion(.success(catFacts))
-            } catch {
-                completion(.failure(NetworkError.unexpectedData))
+        guard let url = Bundle.main.url(forResource: "CatFacts", withExtension: "json") else {
+            completion(.failure(GeneralError.missingJSONFile))
+            return
+        }
+        do {
+            let data = try Data(contentsOf: url)
+            let decoder = JSONDecoder()
+            let jsonData = try decoder.decode(RemoteCatFactResponse.self, from: data)
+            let catFacts = jsonData.data.map { remoteFact in
+                CatFactItem(fact: remoteFact.fact)
             }
+            completion(.success(catFacts))
+        } catch {
+            completion(.failure(GeneralError.unexpectedData))
         }
     }
 }
