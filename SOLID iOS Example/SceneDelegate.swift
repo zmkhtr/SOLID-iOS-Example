20ef501afa7480f6ad567a0b7de1cd9829ca2249
zmkhtr <azammukhtar3@gmail.com>
2023-01-22 17:42:37 +0700

    Implement CatFactListViewController with injected CatFactLoader

--- SOLID iOS Example/SceneDelegate.swift
+++ SOLID iOS Example/SceneDelegate.swift
@@ -8,14 +8,31 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
-
+    
+    
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
-        guard let _ = (scene as? UIWindowScene) else { return }
-    }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
 
+        let url = URL(string: "https://catfact.ninja/facts")!
+        let client = URLSession.init(configuration: .ephemeral)
+        
+        let loader = CatFactLoader(url: url, client: client)
+        
+        window?.rootViewController = makeCatFactsListViewController(with: loader)
+        window?.makeKeyAndVisible()
+    }
+    
+    
+    private func makeCatFactsListViewController(with loader: CatFactLoader) -> CatFactListViewController {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let catFactViewController = storyboard.instantiateInitialViewController { coder in
+            return CatFactListViewController(coder: coder, loader: loader)
+        }
+        return catFactViewController!
+    }
 }
 
