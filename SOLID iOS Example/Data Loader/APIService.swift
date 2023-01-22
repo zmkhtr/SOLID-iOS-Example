cba0fa74e507ad4802f8fefd07ece3dab63f9e7d
zmkhtr <azammukhtar3@gmail.com>
2023-01-22 16:47:23 +0700

    Change hardcoded Mapper to CatFactsItemMapper

--- SOLID iOS Example/Data Loader/APIService.swift
+++ SOLID iOS Example/Data Loader/APIService.swift
@@ -23,16 +23,15 @@ class APIService {
         client.dataTask(with: url) { [weak self] (data, response, error) in
             guard self != nil else { return }
             DispatchQueue.main.sync {
-                do {
-                    if let data = data,
-                       let response = response as? HTTPURLResponse,
-                       response.statusCode == 200 {
-                        let response = try JSONDecoder().decode(RemoteCatFactResponse.self, from: data)
-                        completion(.success(response.data))
-                    } else {
+                if let data = data,
+                   let response = response as? HTTPURLResponse {
+                    do {
+                        let facts = try CatFactsItemMapper.map(data, from: response)
+                        completion(.success(facts))
+                    } catch {
                         completion(.failure(NetworkError.unexpectedData))
                     }
-                } catch {
+                } else {
                     completion(.failure(NetworkError.networkError))
                 }
             }
