6bdd808ace82ca3b91c318e8c9d5293b470eabc4
zmkhtr <azammukhtar3@gmail.com>
2023-01-22 22:06:18 +0700

    Refactor CatFactLoader to make it an template class type

--- SOLID iOS Example/Data Loader/CatFactLoader.swift
+++ SOLID iOS Example/Data Loader/CatFactLoader.swift
@@ -9,36 +9,9 @@ import Foundation
 
 class CatFactLoader {
     
-    private let url: URL
-    private let client: URLSession
-    
     typealias Result = Swift.Result<[CatFactItem], Error>
     
-    init(url: URL, client: URLSession) {
-        self.url = url
-        self.client = client
-    }
-    
-    func load(completion: @escaping (Result) -> Void) {
-        client.dataTask(with: url) { [weak self] (data, response, error) in
-            guard self != nil else { return }
-            if let data = data,
-               let response = response as? HTTPURLResponse {
-                completion(CatFactLoader.map(data, response))
-            } else {
-                completion(.failure(NetworkError.networkError))
-            }
-        }.resume()
-    }
-    
-    private static func map(_ data: Data, _ response: HTTPURLResponse) -> Result {
-        do {
-            let facts = try CatFactsItemMapper.map(data, from: response)
-            return .success(facts)
-        } catch {
-            return .failure(NetworkError.unexpectedData)
-        }
-    }
+    func load(completion: @escaping (Result) -> Void) {}
 }
 
 
