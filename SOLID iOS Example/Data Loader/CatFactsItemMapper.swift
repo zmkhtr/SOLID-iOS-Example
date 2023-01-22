d3a77bfe5b384f98decb848b978c28cde249bb56
zmkhtr <azammukhtar3@gmail.com>
2023-01-22 16:59:31 +0700

    Change CatFactsItemMapper map to return [CatFactItem] instead of [RemoteCatFact]

--- SOLID iOS Example/Data Loader/CatFactsItemMapper.swift
+++ SOLID iOS Example/Data Loader/CatFactsItemMapper.swift
@@ -9,11 +9,14 @@ import Foundation
 
 class CatFactsItemMapper {
 
-    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [RemoteCatFact] {
+    static func map(_ data: Data, from response: HTTPURLResponse) throws -> [CatFactItem] {
         guard response.statusCode == 200, let items = try? JSONDecoder().decode(RemoteCatFactResponse.self, from: data) else {
             throw NetworkError.unexpectedData
         }
 
-        return items.data
+        let catFacts = items.data.map { remoteFact in
+            CatFactItem(fact: remoteFact.fact)
+        }
+        return catFacts
     }
 }
